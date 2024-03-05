d9f6c35b3f93918a98f3253b351aaa3395bd26c5
JungsunPark1 <hype1809@naver.com>
2024-03-05 14:16:19 +0900

    api주소

--- src/lib/api/apiServices.jsx
+++ src/lib/api/apiServices.jsx
@@ -6,7 +6,7 @@ import axios from 'axios';
 // 실시간 강원도 미세먼지 조회 - 	시도별 실시간 측정정보 조회
 export const fetchDustData = async () => {
   const URL =
-    'http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getCtprvnRltmMesureDnsty?serviceKey=Yr6jP9pcxIeobwpMRKwBXBuNVz1pZ3BX3hTp5d7cTL08bO%2F3fudgUVk82zJdmAJ9xFzEmgnlXXLM%2FcGWJgLjtA%3D%3D';
+    'https://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getCtprvnRltmMesureDnsty?serviceKey=Yr6jP9pcxIeobwpMRKwBXBuNVz1pZ3BX3hTp5d7cTL08bO%2F3fudgUVk82zJdmAJ9xFzEmgnlXXLM%2FcGWJgLjtA%3D%3D';
   try {
     const response = await axios(URL, {
       params: {
@@ -24,7 +24,7 @@ export const fetchDustData = async () => {
 // 현재 추천지역 날씨 정보 (기상청) - 기온, 강수량 - 단기예보조회(기상청)
 export const fetchWeatherData = async (nx, ny, specialDate, specialTime) => {
   const URL =
-    'http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst?serviceKey=Yr6jP9pcxIeobwpMRKwBXBuNVz1pZ3BX3hTp5d7cTL08bO%2F3fudgUVk82zJdmAJ9xFzEmgnlXXLM%2FcGWJgLjtA%3D%3D';
+    'https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst?serviceKey=Yr6jP9pcxIeobwpMRKwBXBuNVz1pZ3BX3hTp5d7cTL08bO%2F3fudgUVk82zJdmAJ9xFzEmgnlXXLM%2FcGWJgLjtA%3D%3D';
   try {
     const response = await axios.get(URL, {
       params: {
@@ -51,7 +51,7 @@ export const fetchPastDustData = async (
   msrstnName
 ) => {
   const URL =
-    'http://apis.data.go.kr/B552584/ArpltnStatsSvc/getMsrstnAcctoRDyrg?serviceKey=Yr6jP9pcxIeobwpMRKwBXBuNVz1pZ3BX3hTp5d7cTL08bO%2F3fudgUVk82zJdmAJ9xFzEmgnlXXLM%2FcGWJgLjtA%3D%3D';
+    'https://apis.data.go.kr/B552584/ArpltnStatsSvc/getMsrstnAcctoRDyrg?serviceKey=Yr6jP9pcxIeobwpMRKwBXBuNVz1pZ3BX3hTp5d7cTL08bO%2F3fudgUVk82zJdmAJ9xFzEmgnlXXLM%2FcGWJgLjtA%3D%3D';
   try {
     const response = await axios.get(URL, {
       params: {
